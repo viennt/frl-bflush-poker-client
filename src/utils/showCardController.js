5c640424fe83ea17cc476830d2ca6e280b556625
Trần Quang <ttquang1410@gmail.com>
2020-02-27 21:55:45 +0700

    fix handle show card

--- src/utils/showCardController.js
+++ src/utils/showCardController.js
@@ -1,14 +1,25 @@
 export function handleShowCardParam(payload, currentShowCard) {
-    let showCard = currentShowCard;
-    if (showCard.hasOwnProperty(payload[0])) {
+    let showCard = {...currentShowCard};
+    if (parseFloat(payload[0]) === 0) {
         showCard[payload[0]].push(payload[1])
     } else {
-        showCard[payload[0]] = [payload[1]]
+        if (showCard.hasOwnProperty(payload[0])) {
+            if (showCard[payload[0]].length === 2) {
+                showCard[payload[0]].shift();
+                showCard[payload[0]].push(payload[1])
+            } else {
+                showCard[payload[0]].push(payload[1])
+            }
+        } else {
+            showCard[payload[0]] = [payload[1]]
+        }
     }
+    console.log('Player id: ' + payload[0] + ' has ' + showCard[payload[0]].length );
     return showCard
 }
 
 export function handleHighlightCards(payload, currentShowCard) {
-    currentShowCard[payload[0]] = payload[1].split("+");
-    return currentShowCard;
+    let updateHighLightCard = {...currentShowCard};
+    updateHighLightCard[payload[0]] = payload[1].split("+");
+    return updateHighLightCard;
 }
