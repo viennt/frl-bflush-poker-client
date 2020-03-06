79a2321f6ece43535b82c99792c44e2c4f2475a9
Trần Quang <ttquang1410@gmail.com>
2020-03-06 21:53:57 +0700

    add fullscreen feature

--- src/components/GameHeader/index.js
+++ src/components/GameHeader/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Menu from "./Menu";
 import UpdateStatusGame from "./UpdateStatusGame";
+import ShowFullScreen from "../ShowFullScreen";
 
 const GameHeader = (props) => {
     return <div className="game__header row">
+        <ShowFullScreen/>
         <Menu />
         <div className={'spacer'}/>
         <UpdateStatusGame />
